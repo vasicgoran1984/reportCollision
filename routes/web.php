ce6c99db3dc16245d755b16fde0bd65ae935c5a1
dragor.dev <dragor.dev@gmail.com>
2024-08-30 19:21:52 +0200

    Added a new changes

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('/');
 
 Route::get('/getCity', 'App\Http\Controllers\CityController@index');
 Route::get('startPost', 'App\Http\Controllers\AgreementController@index');
-Route::get('startReport', 'App\Http\Controllers\PrequalificationController@index')->name('startReport');
-Route::get('information', 'App\Http\Controllers\InformationController@index');
-
+Route::get('startReport', 'App\Http\Controllers\PrequalificationController@index');
+Route::get('information', 'App\Http\Controllers\InformationController@index')->name('information');
+Route::get('toPrequalification', 'App\Http\Controllers\PrequalificationController@index')->name('backPrequalification');
+Route::get('drivers', 'App\Http\Controllers\DriversController@index');
